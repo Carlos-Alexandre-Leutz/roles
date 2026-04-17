d94a76890a693c67cd6e648d90fe1950b6696536
Carlos-Alexandre-Leutz <leutzeleutz@gmail.com>
2026-04-17 09:18:47 -0300

    feature: criando ci

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/services/firebase.ts';
@@ -12,7 +12,7 @@ import FriendsView from '@/views/Dashboard/FriendsView.vue';
 import settingsComponent from '@/components/layout/components/settingsComponent.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
